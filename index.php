8e30490bf158bc4767f6b1cd47f04718748ce941
Taufik Akbar Abdullah <taufik@xtremax.com>
2016-10-13 17:57:28 +0700

    fixed firebase 3

--- index.php
+++ index.php
@@ -19,15 +19,6 @@
 require_once('LINEBotTiny.php');
 require_once('firebaseLib.php');
 
-// firebase!!!!!!!!!!!!!!!!!!!!
-// firebase token etc
-$url = "https://cari-tiket-kereta.firebaseio.com";
-$token = "";
-
-// init firebase
-
-// $kota = $firebase->get("stasiun");
-// print_r($kota);
 
 $channelAccessToken = 'wXNwka0cv5nHXaxH8gdAUzE0sLfOqVSV0RaORkWUgdDdXmHn1V2ESqcMwWBH4Mdv+96AqCaewXoBfPJB/sQADtgoi959EjaSoXvFqeMGtBnMLLXJyJVEjOCpNgYQbvNQw5OENcRm6wPuPK+LJB0YdgdB04t89/1O/w1cDnyilFU=';
 $channelSecret = 'ceddb49f9818734f7da2c6cebf522694';
@@ -147,6 +138,11 @@ function processMessage($message, $source){
             $text = "Berikut daftar kota yang dapat dilakukan pencarian: ";
 
             // get kota
+            // firebase!!!!!!!!!!!!!!!!!!!!
+            // firebase token etc
+            $url = "https://cari-tiket-kereta.firebaseio.com";
+            $token = "";
+            
             $firebase = new \Firebase\FirebaseLib($url,$token);
             $kota = $firebase->get("stasiun");
 
